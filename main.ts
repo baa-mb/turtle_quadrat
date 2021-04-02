3756da1a7105e44d345cbaae1919a80cea46741b
baa <34665120+baa-mb@users.noreply.github.com>
2021-04-02 12:49:47 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+turtle.forward(1)
+turtle.forward(1)
+turtle.turnRight()
+turtle.pen(TurtlePenMode.Down)
+for (let index = 0; index < 4; index++) {
+    turtle.forward(1)
+    turtle.forward(1)
+    turtle.turnRight()
+    turtle.forward(1)
+    turtle.forward(1)
+}
+basic.forever(function () {
+	
+})
